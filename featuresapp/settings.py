8bca2e809e4e5209c116b9b6b2aad0310b6f1e71
Michael Avila <bytecollective@gmail.com>
2011-04-06 03:36:00 -0600

    enabled admin

--- featuresapp/settings.py
+++ featuresapp/settings.py
@@ -109,11 +109,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     'south',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
