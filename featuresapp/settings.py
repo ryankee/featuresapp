df940e2529524fc8705f289225efdc49403ce53d
Michael Avila <bytecollective@gmail.com>
2011-04-07 23:35:58 -0600

    personal page used as home page

--- featuresapp/settings.py
+++ featuresapp/settings.py
@@ -107,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -117,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'people',
 )
 
 # A sample logging configuration. The only tangible logging
